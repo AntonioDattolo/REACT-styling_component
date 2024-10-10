44db85ce69dfec68c93c1c524ef83e09720e7cfa
AntonioDattolo <antonio19794.a@gmail.com>
2024-10-10 18:42:14 +0200

    migration to TAILWIND CSS

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -6,12 +6,12 @@ import Button from './BUTTON.JSX';
 
 import Input from './Input.jsx';
 
-const ControlContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  margin-bottom: 1.5rem;
-`
+// const ControlContainer = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   gap: 0.5rem;
+//   margin-bottom: 1.5rem;
+// `
 // TAGGED TEMPLATES è possibile creare un componente in un altro componente con style già integrato
 
 
@@ -38,8 +38,10 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <ControlContainer>
+    <div id="auth-inputs" className='w-full max-w-sm mx-auto p-8 rounded shadow-md bg-gradient-to-b from-stone-700 to-stone-800' >
+      {/* <ControlContainer> */}
+
+      <div className='flex flex-col gap-2 mb-6'>
 
         <Input
           label="Email"
@@ -64,12 +66,12 @@ export default function AuthInputs() {
           // className={passwordNotValid ? 'invalid' : undefined}
           onChange={(event) => handleInputChange('password', event.target.value)}
         />
+      </div>
 
-      </ControlContainer>
-      <div className="actions">
-        <button
+      {/* </ControlContainer> */}
+      <div className="flex justify-end gap-4">
+        <button className="text-amber-400 hover:text-amber-600"
           type="button"
-          className="text-button"
         >
           Create a new account
         </button>
